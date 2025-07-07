9fb0cab6b6a939fc51c20b31fd75ed4acbf30dc4
kratiDatacode <krati.datacode@gmail.com>
2025-07-07 21:09:28 +0530

    add form validation without yup

--- src/components/FormWithoutYup.jsx
+++ src/components/FormWithoutYup.jsx
@@ -14,8 +14,97 @@ const FormWithoutYup = () => {
     birthDate: "",
   });
 
+  const [errors, setErrors] = useState({});
+
+  const isValidEmail = (email) => {
+    //Regular expression for basic email validation
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    return emailRegex.test(email);
+  };
+
+  const isValidPhoneNumber = (phoneNumber) => {
+    //Regular expression for basic phone number validation(10 digits)
+    const phoneRegex = /^\d{10}$/;
+    return phoneRegex.test(phoneNumber);
+  };
+
+  const isValidPassword = (password) => {
+    //Regular expression for password validation
+    const symbolRegex = /[!@#$%^&*(),.?{}|<>]/;
+    const numberRegex = /[0-9]/;
+    const upperCaseRegex = /[A-Z]/;
+    const lowercaseRegex = /[a-z]/;
+    return (
+      password.length >= 8 &&
+      symbolRegex.test(password) &&
+      numberRegex.test(password) &&
+      upperCaseRegex.test(password) &&
+      lowercaseRegex.test(password)
+    );
+  };
+
+  const isValidAge = (age) => {
+    return parseInt(age) >= 18 && parseInt(age) <= 100;
+  };
+  const validateForm = () => {
+    let newErrors = {};
+
+    if (!formData.firstName) {
+      newErrors.firstName = "First name is required";
+    }
+    if (!formData.lastName) {
+      newErrors.lastName = "Last name is required";
+    }
+    if (!formData.email) {
+      newErrors.email = "Email is required";
+    } else if (!isValidEmail(formData.email)) {
+      newErrors.email = "Invalid email formate";
+    }
+    if (!formData.phoneNumber) {
+      newErrors.phoneNumber = "Phone number is required";
+    } else if (!isValidPhoneNumber(formData.phoneNumber)) {
+      newErrors.phoneNumber = "Phone number must be 10 digits";
+    }
+    if (!formData.password) {
+      newErrors.password = "Password is required";
+    } else if (!isValidPassword(formData.password)) {
+      newErrors.password =
+        "Password must be at least 8 characters long and contain at least one symbol,one number,one uppercase letter and one lowercase letter";
+    }
+    if (!formData.confirmPassword) {
+      newErrors.confirmPassword = "Confirm password is required";
+    } else if (formData.confirmPassword !== formData.password) {
+      newErrors.confirmPassword = "Passwords must match";
+    }
+    if (!formData.age) {
+      newErrors.age = "Age is required";
+    } else if (!isValidAge(formData.age)) {
+      newErrors.age =
+        "You must be at least 18 years old and not older than 100 years";
+    }
+    if (!formData.gender) {
+      newErrors.gender = "Gender is required";
+    }
+    if (formData.interests.length === 0) {
+      newErrors.interests = "Select at leasr one interest";
+    }
+    if (!formData.birthDate) {
+      newErrors.birthDate = "Date of birth is required";
+    }
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  console.log(errors);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    const isValid = validateForm();
+    if (isValid) {
+      console.log("form Submitted", formData);
+    } else {
+      console.log("form validation Failed");
+    }
   };
 
   const handleChange = (e) => {
@@ -36,10 +125,10 @@ const FormWithoutYup = () => {
         (interest) => interest !== name
       );
     }
-     
+
     setFormData({
-        ...formData,
-        interests:updatedInterests,
+      ...formData,
+      interests: updatedInterests,
     });
   };
 
@@ -60,6 +149,9 @@ const FormWithoutYup = () => {
                     className="form-control mb-3"
                     onChange={handleChange}
                   />
+                  {errors.firstName && (
+                    <div className="error">{errors.firstName}</div>
+                  )}
                 </div>
                 <div>
                   <label>Last Name:</label>
@@ -71,6 +163,9 @@ const FormWithoutYup = () => {
                     className="form-control mb-3"
                     onChange={handleChange}
                   />
+                   {errors.lastName && (
+                    <div className="error">{errors.lastName}</div>
+                  )}
                 </div>
                 <div>
                   <label>Email:</label>
@@ -82,17 +177,23 @@ const FormWithoutYup = () => {
                     className="form-control mb-3"
                     onChange={handleChange}
                   />
+                   {errors.email && (
+                    <div className="error">{errors.email}</div>
+                  )}
                 </div>
                 <div>
                   <label>Phone Number:</label>
                   <input
-                    type="email"
+                    type="text"
                     name="phoneNumber"
                     value={formData.phoneNumber}
                     placeholder="Enter your Phone Number"
                     className="form-control mb-3"
                     onChange={handleChange}
                   />
+                   {errors.phoneNumber && (
+                    <div className="error">{errors.phoneNumber}</div>
+                  )}
                 </div>
                 <div>
                   <label>Password:</label>
@@ -104,6 +205,9 @@ const FormWithoutYup = () => {
                     className="form-control mb-3"
                     onChange={handleChange}
                   />
+                   {errors.password && (
+                    <div className="error">{errors.password}</div>
+                  )}
                 </div>
                 <div>
                   <label>Confirm Password:</label>
@@ -115,6 +219,9 @@ const FormWithoutYup = () => {
                     className="form-control mb-3"
                     onChange={handleChange}
                   />
+                   {errors.confirmPassword && (
+                    <div className="error">{errors.confirmPassword}</div>
+                  )}
                 </div>
                 <div>
                   <label>Age:</label>
@@ -126,6 +233,9 @@ const FormWithoutYup = () => {
                     className="form-control mb-3"
                     onChange={handleChange}
                   />
+                   {errors.age && (
+                    <div className="error">{errors.age}</div>
+                  )}
                 </div>
                 <div>
                   <label>Gender:</label>
@@ -139,6 +249,9 @@ const FormWithoutYup = () => {
                     <option value="female">Female</option>
                     <option value="other">Other</option>
                   </select>
+                  {errors.gender && (
+                    <div className="error">{errors.gender}</div>
+                  )}
                 </div>
                 <div>
                   <label className="mb-3">Interests:</label>
@@ -169,6 +282,9 @@ const FormWithoutYup = () => {
                     />
                     Reading
                   </label>
+                  {errors.interests && (
+                    <div className="error">{errors.interests}</div>
+                  )}
                 </div>
                 <div>
                   <label>Date of Birth:</label>
@@ -179,6 +295,9 @@ const FormWithoutYup = () => {
                     placeholder="Enter your date of bith"
                     className="form-control mb-3"
                   />
+                    {errors.birthDate && (
+                    <div className="error">{errors.birthDate}</div>
+                  )}
                 </div>
                 <button type="submit" className="btn btn-primary mb-3">
                   Submit
