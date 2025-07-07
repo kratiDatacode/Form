60f62eac9be3592ed0e00ab8a2de67dd25b75ce9
kratiDatacode <krati.datacode@gmail.com>
2025-07-07 18:57:20 +0530

    create basic form

---
+++ src/components/FormWithoutYup.jsx
@@ -0,0 +1,150 @@
+import { useState } from "react";
+
+const FormWithoutYup = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    confirmPassword: "",
+    age: "",
+    gender: "",
+    interests: [],
+    birthDate: "",
+  });
+  const handleSubmit = () => {};
+
+  return (
+    <>
+      <section className="row mx-0 py-5 my-5 d-flex justify-content-center align-items-center">
+        <div className="col-md-10">
+          <div className="row mx-0 d-flex justify-content-center align-items-center">
+            <div className="col-md-10 p-4 rounded shadow bg-white">
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <label>First Name:</label>
+                  <input
+                    type="text"
+                    name="firstName"
+                    value={formData.firstName}
+                    placeholder="Enter your first name"
+                    className="form-control mb-3"
+                  />
+                </div>
+                <div>
+                  <label>Last Name:</label>
+                  <input
+                    type="text"
+                    name="lastName"
+                    value={formData.lastName}
+                    placeholder="Enter your last name"
+                    className="form-control mb-3"
+                  />
+                </div>
+                <div>
+                  <label>Email:</label>
+                  <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    placeholder="Enter your email"
+                    className="form-control mb-3"
+                  />
+                </div>
+                <div>
+                  <label>Phone Number:</label>
+                  <input
+                    type="email"
+                    name="phoneNumber"
+                    value={formData.phoneNumber}
+                    placeholder="Enter your Phone Number"
+                    className="form-control mb-3"
+                  />
+                </div>
+                <div>
+                  <label>Password:</label>
+                  <input
+                    type="password"
+                    name="password"
+                    value={formData.password}
+                    placeholder="Enter your password"
+                    className="form-control mb-3"
+                  />
+                </div>
+                <div>
+                  <label>Confirm Password:</label>
+                  <input
+                    type="password"
+                    name="confirmPassword"
+                    value={formData.confirmPassword}
+                    placeholder="Confirm your password"
+                    className="form-control mb-3"
+                  />
+                </div>
+                <div>
+                  <label>Age:</label>
+                  <input
+                    type="number"
+                    name="age"
+                    value={formData.age}
+                    placeholder="Enter your age"
+                    className="form-control mb-3"
+                  />
+                </div>
+                <div>
+                  <label>Gender:</label>
+                  <select name="gender"  value={formData.gender} className="form-control mb-3">
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                  </select >
+                </div>
+                <div>
+                  <label className="mb-3">Interests:</label>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="coding"
+                      checked={formData.interests.includes("coding")}
+                    />
+                    Coding
+                  </label>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="sports"
+                      checked={formData.interests.includes("sports")}
+                    />
+                    Sports
+                  </label>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="reading"
+                      checked={formData.interests.includes("reading")}
+                    />
+                    Reading
+                  </label>
+                </div>
+                <div>
+                  <label>Date of Birth:</label>
+                  <input
+                    type="date"
+                    name="bithDate"
+                    value={formData.birthDate}
+                    placeholder="Enter your date of bith"
+                    className="form-control mb-3"
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary mb-3">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default FormWithoutYup;
